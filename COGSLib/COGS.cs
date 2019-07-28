a9b7376e01cce8dd7036371afc26341a1980b935
Malchazeen <malchazeen.edge@gmail.com>
2019-07-29 02:51:19 +0700

    turn WeightedAverage into aggregated entry

--- COGSLib/COGS.cs
+++ COGSLib/COGS.cs
@@ -26,56 +26,45 @@ namespace COGSLib
     {
         public COGSResult ComputeCOGS(List<Entry> entries)
         {
-            List<ComputedEntry> computedEntries = new List<ComputedEntry>();
-            Queue<ComputedEntry> inventoryQueue = new Queue<ComputedEntry>();
+            int buyQuantity = 0;
+            decimal buyGrossValue = 0;
+            decimal buyNetValue = 0;
+            DateTime buyDateTime = default;
+            int sellQuantity = 0;
+            decimal sellGrossValue = 0;
+            decimal sellNetValue = 0;
+            DateTime sellDateTime = default;
 
-            int outstandingQuantity = 0;
-            decimal inputValue = 0;
-            decimal unallocatedValue = 0;
-            decimal outstandingValue = 0;
             foreach (var entry in entries)
             {
-                List<EntryRelationship> relatedEntries = new List<EntryRelationship>();
-                var computedEntry = new ComputedEntry(entry, relatedEntries);
-                int detailNoCounter = 0;
                 if (entry.EntryType == EntryType.Buy)
                 {
-                    if (entry.NetValue > unallocatedValue)
-                    {
-                        inputValue += entry.NetValue - unallocatedValue;
-                        unallocatedValue = 0;
-                    }
-                    else
-                    {
-                        unallocatedValue -= entry.NetValue;
-                    }
-                    outstandingQuantity += entry.Quantity;
-                    outstandingValue += entry.NetValue;
-                    inventoryQueue.Enqueue(computedEntry);
+                    buyQuantity += entry.Quantity;
+                    buyGrossValue += entry.GrossValue;
+                    buyNetValue += entry.NetValue;
+                    buyDateTime = entry.EntryDateTime;
                 }
                 else
                 {
-                    int quantity = entry.Quantity;
-                    while (quantity > 0)
-                    {
-                        var currentInventoryEntry = inventoryQueue.Peek();
-
-                        int matchableQuantity = Math.Min(quantity, currentInventoryEntry.RemainingQuantity);
-                        currentInventoryEntry.QuantityConsumed += matchableQuantity;
-                        quantity -= matchableQuantity;
-                        if (currentInventoryEntry.RemainingQuantity == 0)
-                            inventoryQueue.Dequeue();
-
-                        relatedEntries.Add(new EntryRelationship(entry.EntryNo, ++detailNoCounter, currentInventoryEntry.EntryNo, matchableQuantity));
-                    }
-
-                    outstandingValue -= outstandingValue * (entry.Quantity / (decimal)outstandingQuantity);
-                    outstandingQuantity -= entry.Quantity;
-                    unallocatedValue += entry.NetValue;
+                    sellQuantity += entry.Quantity;
+                    sellGrossValue += entry.GrossValue;
+                    sellNetValue += entry.NetValue;
+                    sellDateTime = entry.EntryDateTime;
                 }
-                computedEntries.Add(computedEntry);
             }
 
+            int outstandingQuantity = buyQuantity - sellQuantity;
+            decimal inputValue = buyNetValue;
+            decimal unallocatedValue = sellNetValue;
+            decimal outstandingValue = buyNetValue * (outstandingQuantity / (decimal)buyQuantity);
+
+            List<ComputedEntry> computedEntries = new List<ComputedEntry>
+            {
+                new ComputedEntry(1, buyDateTime, EntryType.Buy, buyQuantity, buyGrossValue, buyNetValue, new List<EntryRelationship>()) { QuantityConsumed = sellQuantity, },
+                new ComputedEntry(2, sellDateTime, EntryType.Sell, sellQuantity, sellGrossValue, sellNetValue, new List<EntryRelationship> {
+                    new EntryRelationship(2, 1, 1, sellQuantity),
+                }),
+            };
             Performance performance = new Performance(outstandingQuantity, inputValue, unallocatedValue, outstandingValue);
             return new COGSResult(performance, computedEntries);
         }
