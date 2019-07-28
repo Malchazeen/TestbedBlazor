554fa7b9dc3df03a6b657ec40424125756331135
Malchazeen <malchazeen.edge@gmail.com>
2019-07-28 14:59:41 +0700

    Add Serverside Blazor

--- TestbedBlazor2.Server/Startup.cs
+++ TestbedBlazor2.Server/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
 using System.Linq;
+using COGSLib;
 
 namespace TestbedBlazor2.Server
 {
@@ -14,12 +15,17 @@ namespace TestbedBlazor2.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddServerSideBlazor();
             services.AddMvc().AddNewtonsoftJson();
             services.AddResponseCompression(opts =>
             {
                 opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
                     new[] { "application/octet-stream" });
             });
+
+            services.AddSingleton<WeightedAverage>();
+            services.AddSingleton<FIFO>();
+            services.AddSingleton<LIFO>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +47,9 @@ namespace TestbedBlazor2.Server
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
-                endpoints.MapFallbackToClientSideBlazor<Client.Startup>("index.html");
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+                //endpoints.MapFallbackToClientSideBlazor<Client.Startup>("index.html");
             });
         }
     }
